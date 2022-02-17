8fe18e735a798b08c54fc7aec53c5d51795519cb
RensSevink <332726@student.mboutrecht.nl>
2022-02-17 12:20:56 +0100

    5.4 af

--- navbar.php
+++ navbar.php
@@ -1,12 +1,5 @@
 <?php
-  if (isset($_GET["content"])) {
-    $active = $_GET["content"];
-  }
-  else {
-    $active = "";
-  }
-
-
+  $active = (isset($_GET["content"]))? $_GET["content"]: "";
 ?>
 
 
@@ -14,29 +7,29 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Yakuza Fans</a>
+    <a class="navbar-brand" href="./index.php?content=home">Yakuza Fans</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link <?php if( $active == "home") {echo "active";} ?>" aria-current="page" href="./index.php?content=home">Home</a>
+          <a class="nav-link <?php echo (in_array($active, ["home", ""]))? "active": "" ?>" aria-current="page" href="./index.php?content=home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php if( $active == "games") {echo "active";} ?>" href="./index.php?content=games">Games</a>
+          <a class="nav-link <?php echo ($active == "games")? "active": "" ?>" href="./index.php?content=games">Games</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php if( $active == "spinoffs") {echo "active";} ?>" href="./index.php?content=spinoffs">Spin-Offs</a>
+          <a class="nav-link <?php echo ($active == "spinoffs")? "active": "" ?>" href="./index.php?content=spinoffs">Spin-Offs</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle <?php echo (in_array($active, ["xbox", "playstation", "pc"]))? "active": "" ?>" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Consoles
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item <?php echo ($active == "xbox")? "active": "" ?>" href="./index.php?content=xbox">Xbox</a></li>
+            <li><a class="dropdown-item <?php echo ($active == "playstation")? "active": "" ?>" href="./index.php?content=playstation">Playstation</a></li>
+            <li><a class="dropdown-item <?php echo ($active == "pc")? "active": "" ?>" href="./index.php?content=pc">PC</a></li>
           </ul>
         </li>
       </ul>
